99ddc2878974a124a42ea2d921cda598af08d200
Bngo <bobi.ngo@staffmatch.com>
2022-09-04 00:02:25 +0200

    Init project with react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+
+      </header>
+
+      <div className="content">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
