6a340985f378ca15d621bf11837fcd9c2d7fbc6f
Bngo <bobi.ngo@staffmatch.com>
2022-09-09 16:21:14 +0200

    Add Direction page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes, Navigate} from "react-router-dom";
 
 import Header from 'components/header/header';
 import AddressForm from "components/pages/AdressForm/address-form";
+import Direction from "components/pages/Direction/Direction";
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ const App = () => {
       <div className="main-content">
         <Routes>
           <Route path="/" element={ <AddressForm/> }/>
+          <Route path="/direction" element={ <Direction/> }/>
           <Route path="*" element={ <Navigate replace to="/" /> }/>
         </Routes>
       </div>
---
+++ src/components/pages/Direction/Direction.tsx
@@ -0,0 +1,11 @@
+import './Direction.scss';
+
+const Direction = () => {
+  return (
+    <div className="direction">
+      <h1>Direction</h1>
+    </div>
+  )
+}
+
+export default Direction;
